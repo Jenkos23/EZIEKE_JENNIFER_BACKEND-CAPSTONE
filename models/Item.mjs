688d6c159c037fdb8f8782856a58d13c2ecac54a
Jenkos <you@example.com>
2025-01-08 10:25:20 -0800

    added a item schema

---
+++ models/Item.mjs
@@ -0,0 +1,13 @@
+// item.js
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+  designer: { type: String, required: true },
+  design_desciption: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+
+const Item = mongoose.model("Item", itemSchema);
+
+export default Item
